2f4791acd7e8028a0470653e2810b16f6d463881
AlinaKlochai <mrs.ali@tuta.io>
2024-12-30 22:25:47 +0100

    add todo with detailed exception

--- src/main/java/org/example/controller/AddToDoController.java
+++ src/main/java/org/example/controller/AddToDoController.java
@@ -4,17 +4,11 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.example.DTO.ToDoCreateRequestDTO;
 import org.example.DTO.ToDoResponseDTO;
-import org.example.DTO.validationErrorDto.ValidationErrorDto;
-import org.example.DTO.validationErrorDto.ValidationErrorsDto;
 import org.example.service.AddToDoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
