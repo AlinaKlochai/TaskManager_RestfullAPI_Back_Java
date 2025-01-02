bd811e848fefd3993360562b2edc38c265a8ed28
AlinaKlochai <mrs.ali@tuta.io>
2025-01-02 15:08:38 +0100

    refactoring 400 on 409

--- src/main/java/org/example/controller/UpdateStatusToDoController.java
+++ src/main/java/org/example/controller/UpdateStatusToDoController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import lombok.AllArgsConstructor;
 import org.example.DTO.appDTO.OneMessageDTO;
 import org.example.service.UpdateToDoStatusService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,7 +20,8 @@ public class UpdateStatusToDoController {
         Object resultFromUpdate = updateToDoStatusService.updateStatus(id);
 
         if(resultFromUpdate instanceof OneMessageDTO) {
-            return ResponseEntity.badRequest().body(resultFromUpdate);
+
+            return ResponseEntity.status(HttpStatus.CONFLICT).body((OneMessageDTO) resultFromUpdate);
         }
 
         return ResponseEntity.ok(resultFromUpdate);
