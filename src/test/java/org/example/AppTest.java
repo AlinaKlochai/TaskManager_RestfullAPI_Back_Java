a8bfa8624390c0507eca52b81e4e0f6b9d210d66
AlinaKlochai <mrs.ali@tuta.io>
2025-01-05 18:21:07 +0100

    add integration test for check context and main class

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,15 +2,17 @@ package org.example;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Unit test for simple App.
  */
+@SpringBootTest(classes = TaskManager_App.class)
 public class AppTest {
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void testAppContextLoads() {
+        assertThat(true).isTrue();
     }
 }
