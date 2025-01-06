da311b685485022cf2ccd4b26e844351b2400416
AlinaKlochai <mrs.ali@tuta.io>
2025-01-06 13:39:19 +0100

    add integration's tests for AddToDoController

--- src/test/java/org/example/controller/AddToDoControllerTest.java
+++ src/test/java/org/example/controller/AddToDoControllerTest.java
@@ -3,13 +3,9 @@ package org.example.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.DTO.ToDoCreateRequestDTO;
-import org.example.DTO.ToDoResponseDTO;
-import org.example.model.State;
-import org.example.service.AddToDoService;
-import org.junit.jupiter.api.BeforeEach;
+import org.example.repository.ToDoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,10 +14,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,41 +32,34 @@ public class AddToDoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private AddToDoService addToDoService;
-    @InjectMocks
-    private AddToDoController addToDoController;
+    @Autowired
     private ObjectMapper objectMapper;
 
-    @BeforeEach
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(addToDoController).build();
-        objectMapper = new ObjectMapper();
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @AfterEach
+    public void cleanUp(){
+        toDoRepository.deleteAll();
     }
+
 
     @Test
     void testAddNewToDo_ShouldReturnCreatedToDoDTOWithStatus() throws Exception {
 
         ToDoCreateRequestDTO toDoCreateRequestDTO = new ToDoCreateRequestDTO("Test description", new Date());
 
-        State openState = State.OPEN;
-
-        ToDoResponseDTO toDoResponseDTO = new ToDoResponseDTO(1L, "Test description", new Date(), openState);
-
-        when(addToDoService.addToDo(any(ToDoCreateRequestDTO.class))).thenReturn(toDoResponseDTO);
-
         mockMvc.perform(post("/todo/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(toDoCreateRequestDTO)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.description").value("Test description"))
                 .andExpect(jsonPath("$.state").value("OPEN"));
     }
 
     @Test
-    void testAddNewToDo_ShouldReturnBadRequestWhenWeSendToDoWithNullInformation() throws Exception {
-
+    void testAddNewToDo_ShouldReturnBadRequestWhenDataIsInvalid() throws Exception {
         ToDoCreateRequestDTO toDoCreateRequestDTO = new ToDoCreateRequestDTO("", null);
 
         mockMvc.perform(post("/todo/")
