4fb8fa7650f0af652cddbb87e4a6e809fd10dd8f
AlinaKlochai <mrs.ali@tuta.io>
2025-01-06 18:24:30 +0100

    add tests for main class

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,7 +2,11 @@ package org.example;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,6 +14,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Unit test for simple App.
  */
 @SpringBootTest(classes = TaskManager_App.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class AppTest {
     @Test
     public void testAppContextLoads() {
